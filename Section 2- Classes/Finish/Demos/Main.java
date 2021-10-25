c621a43555a8cc302cbecdbe86cecabed74f06c9
Moira Leon <moira.leon2@gmail.com>
2021-10-25 15:30:17 -0600

    creating objects

--- Section 2- Classes/Finish/Demos/Main.java
+++ Section 2- Classes/Finish/Demos/Main.java
@@ -1,8 +1,14 @@
 package com.codewithmosh;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
         var employee = new Employee(50_000, 20);
         Employee.printNumberOfEmployees();
         int wage = employee.calculateWage();
