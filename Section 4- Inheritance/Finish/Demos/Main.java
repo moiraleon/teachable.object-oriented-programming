108e217689c6ce96ec1a13d17ff1f6c919496ef0
Moira Leon <moira.leon2@gmail.com>
2021-10-26 21:18:35 -0600

    Down casting

--- Section 4- Inheritance/Finish/Demos/Main.java
+++ Section 4- Inheritance/Finish/Demos/Main.java
@@ -3,6 +3,7 @@ package com.codewithmosh;
 public class Main {
 //println method already calls the to string method
     //inheritancce represents and is a relationship - if a method takes an instance of a class type any other classes that inherit the parent class ->those instances are also valid-- this is called upcasting
+    //they will at run time be accessed as the parent type so if you want to access specific methods you will need to cast it to a specific type-- this is called down casting
     public static void main(String[] args) {
        UIControl[] controls = { new TextBox(), new CheckBox() };
        for (var control : controls)
