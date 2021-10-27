638b21f470a33da86a03ca2aed42e897d8146661
Moira Leon <moira.leon2@gmail.com>
2021-10-26 23:09:16 -0600

    abstract classes and methods

--- Section 4- Inheritance/Finish/Demos/UIControl.java
+++ Section 4- Inheritance/Finish/Demos/UIControl.java
@@ -1,6 +1,6 @@
 package com.codewithmosh;
 
-public abstract class UIControl {
+public abstract class UIControl { //abstract classes keep from being able to instantiate classes- methods can be declared abstract for reinforcement
   private boolean isEnabled = true;//other keywords such as protected can be used but are considered bad practice-- protected members are public within their own packages
 
 //  public UIControl(boolean isEnabled) {
