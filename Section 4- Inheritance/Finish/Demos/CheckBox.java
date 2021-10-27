13c8d8eb929742ee8bb5bf25ca4b0ae2f79dd494
Moira Leon <moira.leon2@gmail.com>
2021-10-26 23:12:46 -0600

    final classes and methods

--- Section 4- Inheritance/Finish/Demos/CheckBox.java
+++ Section 4- Inheritance/Finish/Demos/CheckBox.java
@@ -1,6 +1,6 @@
 package com.codewithmosh;
 
-public final class CheckBox extends UIControl {
+public final class CheckBox extends UIControl { //declaring a class as final prevent sit from being extended -not often used- final classes also can not be overridden
 //  UIControl[] controls =  {new TextBox(),new CheckBox()};
 //  for(var control: controls){
 //    ///code
